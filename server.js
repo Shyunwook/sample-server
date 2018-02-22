df2292151eae271a688e2be9dd85f4a373a7dd76
shin hw <gdnfl0117@gmail.com>
2018-02-22 17:51:55 +0900

    2018.02.22

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
+
 var app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sample-server');
+const User = mongoose.model('User', { id: String, password: String, name: String, email: String,
+gender: String, location: String });
 
 // app.use(function (req, res, next) {
 //   console.log('Time:', Date.now());
@@ -11,34 +17,82 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/',function(req,res){
-  res.send('Hello World!!!!!!!!')
-});
+// app.get('/',function(req,res){
+//   res.send('Hello World!!!!!!!!')
+// });
 
 app.get('/hello',function(req,res){
-  res.send('Hello')
+  res.send('Hello');
 });
+
+
+//-------------------------------------------------------------------------------------------------
 
 app.post('/user',function(req,res){
   // res.send('POST (Create)');
-  console.log('데이터 확인',req.body);
-  // post로 날리면 request의 body로 들어온다.
-  res.send({state: 'OK',data: req.body});
+  var data = req.body;
+  const person = new User({ id: data.id, password: data.password, name: data.name, email: data.email,
+  gender: data.gender, location: data.location });
+  person.save().then((result) => {
+    console.log(result),
+    res.send({state: 'OK',data: req.body});
+  });
+  // console.log('데이터 확인',req.body);
+  // // post로 날리면 request의 body로 들어온다.
+  // res.send({state: 'OK',data: req.body});
 });
 
-// app.get('/user/:userId',function(req,res){
-//   // res.send('GET (Read)');
-//   console.log(req.params.userId + '의 정보를 가져옵니다');
-//
-//   var user = {
-//     userId : req.params.userId,
-//     name : 'Shin',
-//     email : 'wow@wow',
-//     company : 'GS'
-//   };
-//
-//   res.send(user);
-// });
+app.post('/login',function(req,res){
+  // res.send('POST (Create)');
+  var data = req.body;
+  const person = new User({ id: data.id, password: data.password});
+  User.findOne({id:data.id, password: data.password},'id name',function(err,result){
+      if (err){
+        console.log('error occured in the database');
+      }
+      if(result){
+        // res.send(result.name + '님 환영합니다.');
+        // console.log(result);
+        res.redirect('/modify.html?id='+result.id);
+      }else{
+        res.send('정보와 일치하는 회원이 존재하지 않습니다.');
+      }
+  });
+});
+
+
+app.get('/user/:userId',function(req,res){
+  // res.send('GET (Read)');
+  User.findOne({id:req.params.userId},'id password name email gender location',function(err,result){
+      if (err){
+        console.log('error occured in the database');
+      }
+      if(result){
+        res.send(result);
+        console.log(result);
+      }else{
+        res.send('정보와 일치하는 회원이 존재하지 않습니다.');
+      }
+  });
+});
+
+app.put('/user/:userId',function(req,res){
+  User.findOneAndUpdate({id:req.params.userId},{name:req.body.name},function(err){
+    if (err)
+        console.log('error occured in the database');
+    res.send('변경완료');
+  })
+});
+
+
+app.delete('/user/:userId',function(req,res){
+  User.deleteOne({id:req.params.userId}, function (err) {
+    if (err)
+      console.log('error occured in the database');
+    res.send('삭제완료');
+  });
+})
+
 
 app.get('/user/search',function(req,res){
   console.log('데이터 확인',req.query.userId);
@@ -51,13 +105,6 @@ app.get('/user/search',function(req,res){
   res.send({result : user});
 });
 
-app.put('/user/:userId',function(req,res){
-  res.send('PUT (Update)');
-});
-
-app.delete('/user/:userId',function(req,res){
-  res.send('DELeTE (Delete)');
-})
 
 app.use(express.static('public'));
 // 정적인 결과를 항상 돌려주는 기능, express의 강력한 기능이다!
